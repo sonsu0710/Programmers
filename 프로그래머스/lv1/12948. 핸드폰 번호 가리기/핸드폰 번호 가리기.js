d86146e9899b4450720a9f49e7b014224f438a29
Sonsu <108868573+sonsu0710@users.noreply.github.com>
2022-11-18 15:12:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,8 +1,3 @@
 function solution(phone_number) {
-    let spliceNum = phone_number.split('').splice(0, phone_number.length-4);
-    let spliceNumLast = phone_number.split('').splice(phone_number.length-4, phone_number.length);
-    spliceNum.forEach((item,idx) => {
-        spliceNum[idx] = '*'
-    })
-    return [...spliceNum, ...spliceNumLast].join('');
+    return phone_number.replace(/\d(?=\d{4})/g,'*');
 }
