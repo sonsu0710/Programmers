c6a22a6042af19aae79f8f8aa2157f844a5e5a7f
Sonsu <108868573+sonsu0710@users.noreply.github.com>
2022-11-26 12:24:20 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,14 @@
+function solution(strings, n) {
+    strings.sort((a, b) => {
+        if (a[n] === b[n]) {
+            if (a > b) return 1;
+            else if (a < b) return -1;
+            else return 0;
+        } else {
+            if (a[n] > b[n]) return 1;
+            else if (a[n] < b[n]) return -1;
+            else return 0;
+        }
+    })
+    return strings;
+}
